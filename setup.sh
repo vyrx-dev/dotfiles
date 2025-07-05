f36ccf38e2aa58cdac63d789f1dd6b66144f0df2
Amit <theamit.969@gmail.com>
2025-07-05 17:20:51 +0530

    added nvim script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 # !/bin/bash
 
-echo "🔗 Setting up symlinks for dotfiles..."
+echo "Setting up symlinks for dotfiles..."
 
 # Zsh config
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
@@ -11,8 +11,15 @@ mkdir -p ~/.config/hypr
 ln -sf ~/dotfiles/config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -sf ~/dotfiles/config/hypr/keybindings.conf ~/.config/hypr/keybindings.conf
 
+# Neovim config
+mkdir -p ~/.config/nvim
+ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+
 # Scripts
 mkdir -p ~/.local/bin
 ln -sf ~/dotfiles/bin/gcal_notify.sh ~/.local/bin/gcal_notify.sh
 
 echo "✅ Symlinks created!"
+
+
+
