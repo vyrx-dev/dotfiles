9963552a98cebbb0cf007740e1c13a9f17f2f3ce
Amit <theamit.969@gmail.com>
2025-07-20 00:54:34 +0530

    added keymaps

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Keymap helper
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- jj to escape insert mode
+keymap.set("i", "jj", "<Esc>", opts)
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
