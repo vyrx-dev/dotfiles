eb7d38eff3fbb308c871794a3e391131f7844075
Amit <theamit.969@gmail.com>
2025-07-22 00:47:49 +0530

    now it is  easier to change  colorschemes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,59 @@
 return {
-  {
-    "craftzdog/solarized-osaka.nvim",
-    branch = "osaka",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
-  },
+	-- Solarized Osaka
+	{
+		"craftzdog/solarized-osaka.nvim",
+		branch = "osaka",
+		lazy = true,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+	},
+	-- Kanagawa
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = {
+			transparent = true,
+			theme = "wave",
+		},
+	},
+	-- Gruvbox
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = {
+			-- Added optional setting for consistency
+			transparent_mode = true,
+		},
+	},
+	-- Catppuccin (pre-installed, added for custom settings)
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+		},
+	},
+	-- Tokyo Night (pre-installed, added for custom settings)
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = {
+			style = "night",
+			transparent = true,
+		},
+	},
+	-- Set the active theme
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "solarized-osaka", -- Change to kanagawa, catppuccin, tokyonight, or solarized-osaka
+		},
+	},
 }
