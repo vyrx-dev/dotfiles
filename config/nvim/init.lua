8ade7a45ce829ba02593271ad44456bb7565cc40
Amit <theamit.969@gmail.com>
2025-07-26 03:56:19 +0530

    updated

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,14 +1,7 @@
 -- Enable Lua module caching (faster startup in Neovim 0.9+)
 if vim.loader then
-  vim.loader.enable()
+	vim.loader.enable()
 end
-
--- Global debug print function (use: dd() or vim.print())
--- Shows tables and nested data in a readable format
-_G.dd = function(...)
-  require("util.debug").dump(...)
-end
-vim.print = _G.dd
 
 -- Load LazyVim and plugin config
 require("config.lazy")
