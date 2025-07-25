d5c666dd5a90a58a853359265fc7a7b90358fc62
Amit <theamit.969@gmail.com>
2025-07-25 07:39:37 +0530

    call pacekeeper

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local pacekeeper = require("doublek.pacekeeper")
+pacekeeper.chill()
+
 -- Keymap helper
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
