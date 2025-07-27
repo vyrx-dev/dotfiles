088f14f696312a3d9834e71c1e158dc5b0cd8b4e
Amit <theamit.969@gmail.com>
2025-07-27 07:32:53 +0530

    markdown preview

---
+++ config/nvim/lua/plugins/mdpreview.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"toppair/peek.nvim",
+		build = "deno task --quiet build:fast",
+		opts = {
+			theme = "light",
+		},
+		keys = {
+			{
+				"<leader>cp",
+				function()
+					require("peek").open()
+				end,
+			},
+		},
+	},
+}
