a553fcc721109449c9b0bd7afb0160cfb02e0b0c
Amit <theamit.969@gmail.com>
2025-08-04 00:12:26 +0530

    updated

--- config/nvim/lua/plugins/mdpreview.lua
+++ config/nvim/lua/plugins/mdpreview.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		"toppair/peek.nvim",
-		build = "deno task --quiet build:fast",
-		opts = {
-			theme = "light",
-		},
-		keys = {
-			{
-				"<leader>cp",
-				function()
-					require("peek").open()
-				end,
-			},
-		},
-	},
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    opts = {
+      theme = "light",
+    },
+    keys = {
+      {
+        "<leader>cp",
+        function()
+          require("peek").open()
+        end,
+      },
+    },
+  },
 }
