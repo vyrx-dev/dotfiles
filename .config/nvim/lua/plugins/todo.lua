5642074898698b483a8f0b8febd61be840d79925
Amit <theamit.969@gmail.com>
2025-08-25 00:11:30 +0530

    updated

---
+++ .config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,13 @@
+return {
+  "vimichael/floatingtodo.nvim",
+  config = function()
+    require("floatingtodo").setup({
+      target_file = "~/ocuments/Obsidian-Notes/todo.md",
+      border = "single", -- single, rounded, etc.
+      width = 0.8, -- width of window in % of screen size
+      height = 0.8, -- height of window in % of screen size
+      position = "center", -- topleft, topright, bottomleft, bottomright
+    })
+    vim.keymap.set("n", "<leader>td", ":Td<CR>", { silent = true })
+  end,
+}
