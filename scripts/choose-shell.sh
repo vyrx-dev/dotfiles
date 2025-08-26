e9bb6d0fee2f5443021650cfbff9beb64790bc69
Amit <theamit.969@gmail.com>
2025-08-26 20:21:54 +0530

    updated

--- scripts/choose-shell.sh
+++ scripts/choose-shell.sh
@@ -14,31 +14,31 @@ print_choice() {
 }
 
 print_info "Choose your user's default shell:"
-echo "1. /bin/bash"
-echo "2. /bin/fish"
-echo "3. /bin/zsh"
+echo "1. bash"
+echo "2. fish"
+echo "3. zsh"
 echo "4. quit"
 
 read -p "Enter the number of your preferred shell: " choice
 
 case $choice in
 1)
-  chsh -s /bin/bash
-  print_choice "Shell choice: /bin/bash"
+  chsh -s /usr/bin/bash
+  print_choice "Shell choice: bash"
   printf '%s\n' "If default shell was changed, you will need to logout and 
 log back in for change to take effect."
   ;;
 2)
-  chsh -s /bin/fish
-  print_choice "Shell choice: /bin/fish"
+  chsh -s /usr/bin/fish
+  print_choice "Shell choice: fish"
   printf '%s\n' "If default shell was changed, you will need to logout and 
 log back in for change to take effect."
   ;;
 3)
-  chsh -s /bin/zsh
-  print_choice "Shell choice: /bin/zsh"
+  chsh -s /usr/bin/zsh
+  print_choice "Shell choice: zsh"
   printf '%s' "If default shell was changed, you will need to logout and 
-log back in fo\nr change to take effect."
+log back in for change to take effect."
   ;;
 4)
   echo "User has chosen to quit program."
