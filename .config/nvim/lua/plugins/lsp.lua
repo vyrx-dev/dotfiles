5b6d2cc90c3587cda1448a0c04ae956782b74efb
Amit <theamit.969@gmail.com>
2025-09-15 20:30:55 +0530

    cleanup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "stylua",
-        "selene",
         "luacheck",
         "shellcheck",
         "shfmt",
